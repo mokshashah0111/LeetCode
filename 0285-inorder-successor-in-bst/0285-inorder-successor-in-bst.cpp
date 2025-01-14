a27986112271bc048011c175be3df82c0e418b62
Moksha Shah <139951624+mokshashah0111@users.noreply.github.com>
2025-01-14 13:48:38 -0800

    Time: 23 ms (48.9%), Space: 23.1 MB (41.48%) - LeetHub

--- 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.cpp
+++ 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.cpp
@@ -6,27 +6,21 @@
  *     TreeNode *right;
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
-
- 1,2,3,4,5,6
  */
-
 class Solution {
-    private:
-    void inorder(TreeNode* root, vector<TreeNode*>& vec){
-        if(!root)return;
-        inorder(root->left,vec);
-        vec.emplace_back(root);
-        inorder(root->right, vec);
-    }
 public:
     TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
-        vector<TreeNode*> result;
-        inorder(root,result);
-        for(int i = 0;i<result.size();i++){
-            if(result[i] == p){
-                if(i+1 < result.size()) return result[i+1];
+        TreeNode* next = nullptr;
+
+        while(root){
+            if(p->val >= root->val){
+                root=root->right;
+            }
+            else{
+                next = root;
+                root= root->left;
             }
         }
-        return nullptr;
+        return next;
     }
 };
