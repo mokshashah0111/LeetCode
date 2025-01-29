a5af6fabf5ae5a48780bc8101a6760e4bc1a8c4d
Moksha Shah <139951624+mokshashah0111@users.noreply.github.com>
2025-01-28 22:25:42 -0800

    Time: 0 ms (100%), Space: 8.6 MB (45.28%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+       double result = 1.0;
+       long long power = n;
+       if(power<0){
+            power =-power;
+            x=1/x;
+        }
+        while(power){
+            if(power&1){
+                result *= x;
+            }
+            x *=x;
+            power >>=1;
+        }
+        return result;
+    }
+};
