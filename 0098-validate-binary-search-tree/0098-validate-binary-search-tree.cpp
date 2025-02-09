1b54f184bed7078bec9be7209fe3b21a43feb77e
Moksha Shah <139951624+mokshashah0111@users.noreply.github.com>
2025-02-08 21:59:39 -0800

    Time: 0 ms (100%), Space: 22.5 MB (9.49%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -15,8 +15,28 @@ class Solution {
         else if(root->val <= min || root->val>=max) return false;
         return validation(root->left,root->val,min) && validation(root->right,max,root->val);
     }
+    struct bfsQueue{
+        TreeNode* node;
+        long lower;
+        long upper;
+    };
 public:
     bool isValidBST(TreeNode* root) {
-        return validation(root,LLONG_MAX,LLONG_MIN);
+        if(!root) return true;
+        queue<bfsQueue>q;
+        q.push(bfsQueue(root,LLONG_MIN,LLONG_MAX));
+        while(!q.empty()){
+            auto currentNode = q.front();
+            q.pop();
+            if(currentNode.node->val <= currentNode.lower || currentNode.node->val >= currentNode.upper) return false;
+            if(currentNode.node->left){
+                q.push(bfsQueue(currentNode.node->left,currentNode.lower,currentNode.node->val));
+            }
+            if(currentNode.node->right){
+                q.push(bfsQueue(currentNode.node->right,currentNode.node->val,currentNode.upper));
+            }
+        }
+        return true;
+
     }
 };
