6e6b361f7c48fccaf68e2a8ea851dc470edffc69
Moksha Shah <139951624+mokshashah0111@users.noreply.github.com>
2025-03-01 15:25:48 -0800

    Time: 48 ms (64.45%), Space: 29.1 MB (66.78%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums){
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>result;
+        for(int i =0; i<nums.size();i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            int low = i+1;
+            int high = nums.size()-1;
+            while(low<high){
+                int sum = nums[i] + nums[low] + nums[high];
+                if(sum== 0){
+                    result.push_back({nums[i],nums[low],nums[high]});
+                    while(low<high && nums[low] == nums[low+1]) low++;
+                    while(low>high && nums[high] == nums[high-1]) high--;
+                    low++;
+                    high--;
+                }
+                else if(sum >0) high--;
+                else low++;
+            }
+        }
+        return result;
+    }
+};
