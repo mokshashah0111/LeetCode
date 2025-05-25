3051cce8db64d58d5e9ae61216e1dc9c7ad85bde
Moksha Shah <139951624+mokshashah0111@users.noreply.github.com>
2025-05-25 10:35:27 -0700

    Time: 0 ms (100%), Space: 8.3 MB (56.94%) - LeetHub

--- 3275-minimum-number-of-pushes-to-type-word-i/3275-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/3275-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        if(word.size() <= 8) return word.size();
-        unordered_map<char,int>push;
-        int initial =0;
+        int initial=0;
         int index =0;
+        int minPush=0;
 
-        while(index<word.size()){
-            if(index%8==0) initial++;
-            if(push.find(word[index]) == push.end()){
-                push[word[index]] = initial;
-            }
-            index++;
-        }
-        int minPush =0;
-        for(auto& c: word){
-            minPush+=push[c];
+        for(int i =0;i<word.size();i++){
+            if(i%8==0)initial++;
+            minPush+=initial;
         }
         return minPush;
     }
