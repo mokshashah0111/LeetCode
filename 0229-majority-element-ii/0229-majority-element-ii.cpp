1cd71e390be373b1fc9a39365ebcc77ef0a8e430
Moksha Shah <139951624+mokshashah0111@users.noreply.github.com>
2025-06-23 12:10:43 -0700

    Time: 0 ms (100%), Space: 23.5 MB (85.02%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        // sort(nums.begin(),nums.end());
         int vote1 =0;
         int vote2 =0;
         int max1 =0;
         int max2 = 0;
 
         for(int& num: nums){//2..1..1..3
-            if(vote1==0){
-                max1 = num;//2
-                vote1++;//0
-            }
-            else if(max1 == num) vote1++;
-            else if(vote2 ==0){
-                max2 = num;//1
-                vote2++;
-            }
-            else if(num==max2)vote2++;//1
-            else{
-                vote2--;vote1--;//1,0
-            }
+            if(num==max1) vote1++;
+            else if(num == max2) vote2++;
+            else if(vote1 ==0)max1 = num,vote1++;
+            else if(vote2 ==0) max2 = num,vote2++;
+            else vote1--,vote2--;    
         }
         vote1 =0,vote2 =0;
         for(int& num: nums){
