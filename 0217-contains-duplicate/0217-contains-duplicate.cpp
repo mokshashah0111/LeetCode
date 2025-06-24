dd74ba3690f45fa30918b085436eeb66f772a22d
Moksha Shah <139951624+mokshashah0111@users.noreply.github.com>
2025-06-24 15:31:45 -0700

    Time: 21 ms (89.94%), Space: 70.8 MB (99.58%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        return nums.size() != set(nums.begin(),nums.end()).size();
+        sort(nums.begin(),nums.end());
+        for(int i =0 ;i<nums.size()-1;i++){
+            if(nums[i] == nums[i+1]) return true;
+        }
+        return false;
     }
 };
