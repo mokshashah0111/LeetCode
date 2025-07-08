50674903dc2529b708147017cfdda21dbb70d8d6
Moksha Shah <139951624+mokshashah0111@users.noreply.github.com>
2025-07-08 16:29:00 -0700

    Time: 56 ms (52.95%), Space: 72.9 MB (40.86%) - LeetHub

--- 1558-course-schedule-iv/1558-course-schedule-iv.cpp
+++ 1558-course-schedule-iv/1558-course-schedule-iv.cpp
@@ -1,37 +1,40 @@
 class Solution {
 public:
     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+        vector<vector<bool>>isReachable(numCourses,vector<bool>(numCourses,false));
         vector<vector<int>>adj(numCourses);
-        for(auto& it: prerequisites){
+        vector<int>inDegrees(numCourses);
+        for(auto it: prerequisites){
             adj[it[0]].emplace_back(it[1]);
+            inDegrees[it[1]]++;
+        }
+        // for(int i =0; i<numCourses;i++){
+        //     for(int j: adj[i]){
+        //         inDegrees[j]++;
+        //     }
+        // }
+        
+        queue<int>q;
+        for(int i =0; i<numCourses;i++){
+            if(inDegrees[i] ==0)q.emplace(i);
+        }
+
+        while(!q.empty()){
+            auto current = q.front(); q.pop();
+            for(auto it: adj[current]){
+                isReachable[current][it] = true;
+                for(int i =0;i<numCourses;i++){
+                   if(isReachable[i][current]){
+                        isReachable[i][it]= true;
+                   }
+                }
+                inDegrees[it]--;
+                if(inDegrees[it] ==0)q.emplace(it);
+            }
         }
         vector<bool>result;
-        vector<bool>visited(numCourses,false);
-
-        for(auto& qr: queries){
-            int current = qr[0];
-            int target = qr[1];
-            queue<int>q;
-            // q.clear();
-            fill(visited.begin(),visited.end(),false);
-            q.emplace(current);
-
-            visited[current] = true;
-            bool found = false;
-            while(!q.empty()){
-                int top = q.front();q.pop();
-                if(top == target){
-                    found = true;
-                    break;
-                }
-                for(auto it: adj[top]){
-                    if(!visited[it]){
-                        q.emplace(it);
-                        visited[it] = true;
-                    }
-                }
-            }
-            result.emplace_back(found);
+        for(auto it: queries){
+            result.emplace_back(isReachable[it[0]][it[1]]);
         }
         return result;
     }
